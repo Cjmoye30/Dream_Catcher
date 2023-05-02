26d4f8fbd8c4d905690067559a2b7527d9bae865
CGoddard1 <goddard.cole@gmail.com>
2023-05-01 22:09:36 -0400

    add nodemon, psudo code journal route and journal template

--- controllers/journalRoutes.js
+++ controllers/journalRoutes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-
+    //figure out the current user (get this from the req.session, to do this we need to add user ID to req.session login (in example code that Jack pushed))
+    //add journal entry model, get all the journal entries for the current user (uses journal.js model)
+    //pass that data to the journal page in res.render, this will all be a dependency on login route and journal model
     res.render('journalpage');
 });
 
 module.exports = router;
+
+//
